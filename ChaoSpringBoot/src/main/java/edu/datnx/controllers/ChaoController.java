9fc664d23aca412dd29febc9cf7038a67010eaba
datx24 <xuandat475@gmail.com>
2025-03-04 10:50:17 +0700

    truyền dữ liệu từ controller sang view

--- ChaoSpringBoot/src/main/java/edu/datnx/controllers/ChaoController.java
+++ ChaoSpringBoot/src/main/java/edu/datnx/controllers/ChaoController.java
@@ -1,12 +1,16 @@
 package edu.datnx.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class ChaoController {
 	@RequestMapping("/hello")
-	public String xinChao() {
+	public String xinChao(ModelMap m) {
+		//Gói dữ liệu sang view
+		m.addAttribute("tb", "Dữ liệu thông báo! Xin Chào SpringBoot");
 		return "index";
 	}
 }
