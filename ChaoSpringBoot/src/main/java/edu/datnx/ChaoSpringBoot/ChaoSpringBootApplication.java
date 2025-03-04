77d950fc271d7ab1f066603a7f1242a839bb85cb
datx24 <xuandat475@gmail.com>
2025-03-04 10:26:40 +0700

    scan package controllers

--- ChaoSpringBoot/src/main/java/edu/datnx/ChaoSpringBoot/ChaoSpringBootApplication.java
+++ ChaoSpringBoot/src/main/java/edu/datnx/ChaoSpringBoot/ChaoSpringBootApplication.java
@@ -2,8 +2,10 @@ package edu.datnx.ChaoSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"edu.datnx.controllers"})
 public class ChaoSpringBootApplication {
 
 	public static void main(String[] args) {
