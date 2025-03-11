4677d98e47b81c0c0db896ac6443398586eafbba
datx24 <xuandat475@gmail.com>
2025-03-11 15:30:01 +0700

    cập nhật css và chỉnh để clean code việc hiển thị danh sách

--- SB_TruyenDuLieuSangView/src/main/java/edu/datnx/controllers/TruyenDuLieuController.java
+++ SB_TruyenDuLieuSangView/src/main/java/edu/datnx/controllers/TruyenDuLieuController.java
@@ -4,24 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import edu.datnx.models.SinhVien;
 import edu.datnx.service.SinhVienService;
 
 @Controller
 public class TruyenDuLieuController {
-	@Autowired
-	SinhVienService sinhVienService;
+
+	SinhVienService sinhVienService = new SinhVienService();
 	
 	@GetMapping("/aboutme")
-	public String truyenDuLieu(ModelMap modelMap) {
+	public String truyenObject(ModelMap modelMap) {
 		SinhVien sv1 = new SinhVien("64130299", "Nguyễn Xuân Đạt", "Nam", "2004");
 		modelMap.addAttribute("sv",sv1);
 		return "index";
 	}
 
-	@GetMapping("/danhsach")
+	@GetMapping("/danhsachsinhvien")
 	public String truyendsObject(ModelMap m) {
 		m.addAttribute("dssv",sinhVienService.getDSSV());
 		return "danhsach";
--- SB_TruyenDuLieuSangView/src/main/java/edu/datnx/service/SinhVienService.java
+++ SB_TruyenDuLieuSangView/src/main/java/edu/datnx/service/SinhVienService.java
@@ -6,13 +6,13 @@ import org.springframework.stereotype.Service;
 
 import edu.datnx.models.SinhVien;
 
-@Service
 public class SinhVienService {
 	public List<SinhVien> getDSSV() {
 		List<SinhVien> dssv = List.of(
-			new SinhVien("111", "Nguyễn Văn An", "Nam", "2012"),
-			new SinhVien("123", "Nguyễn Xuân Đạt", "Nam", "2001"),
-			new SinhVien("231", "Nguyễn Văn Phúc", "Nam", "2004")
+			new SinhVien("64130299", "Nguyễn Văn An", "Nam", "2004"),
+			new SinhVien("66612383", "Nguyễn Xuân Đạt", "Nam", "2006"),
+			new SinhVien("65623414", "Trần Văn Phúc", "Nam", "2005"),
+			new SinhVien("68235243", "Mai Thị Hoa", "Nữ", "2008")
 		);
 		return dssv;
 	}
