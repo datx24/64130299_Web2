043a95a54f933b3cefa211806253715fc1017caa
datx24 <xuandat475@gmail.com>
2025-03-13 23:55:46 +0700

    SB_BMI: cài đặt tính bmi và thêm css

--- SB_BMI/src/main/java/edu/datnx/controllers/BMIController.java
+++ SB_BMI/src/main/java/edu/datnx/controllers/BMIController.java
@@ -1,8 +1,11 @@
 package edu.datnx.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/bmi")
@@ -11,5 +14,27 @@ public class BMIController {
 	@GetMapping("/home")
 	public String showForm() {
 		return "bmi";
+	}
+	
+	@PostMapping("/calculate")
+	public String calculateBMI(@RequestParam double weight, @RequestParam double height, Model m) {
+		double bmi = weight / (height * height);
+		String category = getCategory(bmi);
+		m.addAttribute("result", String.format("%.2f", bmi));
+		m.addAttribute("type",category);
+		return "bmi";
+	}
+	
+	public String getCategory(double bmi) {
+		if(bmi >= 0 && bmi < 18.5) {
+			return "Underweight";
+		}
+		else if(bmi < 24.9) {
+			return "Normal Weight";
+		}
+		else if(bmi < 29.9) {
+			return "Overweight";
+		}
+		return "Obesity";
 	}
 }
