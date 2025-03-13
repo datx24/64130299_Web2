99e31ae68bedfe1235736d2ebc032e0180f4591d
datx24 <xuandat475@gmail.com>
2025-03-13 21:37:20 +0700

    Cài đặt UserSer

---
+++ SB_Login/src/main/java/edu/datnx/services/UserService.java
@@ -0,0 +1,25 @@
+package edu.datnx.services;
+
+import org.springframework.stereotype.Service;
+
+import edu.datnx.models.User;
+import edu.datnx.repositories.UserRepository;
+
+@Service
+public class UserService {
+	private final UserRepository userRepository;
+
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
+	//Phương thức xác thực người dùng trước khi đăng nhập
+	public User authenticate(String id, String password) {
+		User user = userRepository.findUserById(id);
+		if(user != null && user.getPassword().equals(password)) {
+			return user;
+		}
+		return null;
+	}
+	
+}
