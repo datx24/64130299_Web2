c20e1a4a27e6bddcee0b3c827655ea617e3e1642
datx24 <xuandat475@gmail.com>
2025-03-25 09:32:08 +0700

    tạo lớp student

---
+++ TongHopGK/src/main/java/edu/datnx/TongHopGK/models/Student.java
@@ -0,0 +1,37 @@
+package edu.datnx.TongHopGK.models;
+
+public class Student {
+    private String mssv;         
+    private String fullName;     
+    private double averageGrade; 
+
+    public Student(String mssv, String fullName, double averageGrade) {
+        this.mssv = mssv;
+        this.fullName = fullName;
+        this.averageGrade = averageGrade;
+    }
+
+    public String getMssv() {
+        return mssv;
+    }
+
+    public void setMssv(String mssv) {
+        this.mssv = mssv;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public double getAverageGrade() {
+        return averageGrade;
+    }
+
+    public void setAverageGrade(double averageGrade) {
+        this.averageGrade = averageGrade;
+    }
+}
