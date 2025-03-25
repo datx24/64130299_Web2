ac632c9f1429ea8a96255be823fb0eb8cb12916f
datx24 <xuandat475@gmail.com>
2025-03-25 10:00:54 +0700

    push lần 6

--- TongHopGK/src/main/java/edu/datnx/TongHopGK/controller/HomeController.java
+++ TongHopGK/src/main/java/edu/datnx/TongHopGK/controller/HomeController.java
@@ -2,6 +2,7 @@ package edu.datnx.TongHopGK.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import edu.datnx.TongHopGK.services.StudentService;
@@ -23,8 +24,15 @@ public class HomeController {
 	public String about() {
 		return "pages/about";
 	}
+	
+	@GetMapping("/addnew")
+	public String addNew() {
+		return "pages/addNew";
+	}
+	
 	@GetMapping("/studentlist")
-	public String studentlist() {
+	public String studentlist(ModelMap m) {
+		m.addAttribute("students", studentService.getAllStudents());
 		return "pages/studentlist";
 	}
 }
