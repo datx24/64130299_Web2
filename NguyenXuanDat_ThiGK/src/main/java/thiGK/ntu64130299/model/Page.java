8dd9f0612f91d03ed6aa7fe3bcd03da79a09120f
datx24 <xuandat475@gmail.com>
2025-04-01 10:05:01 +0700

    code hiển thị danh sách page

--- NguyenXuanDat_ThiGK/src/main/java/thiGK/ntu64130299/model/Page.java
+++ NguyenXuanDat_ThiGK/src/main/java/thiGK/ntu64130299/model/Page.java
@@ -1,5 +1,6 @@
 package thiGK.ntu64130299.model;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,28 +10,21 @@ public class Page {
 	private String keyWord;
 	private String content;
 	private int parentPageId;
-	public Page(String id, String pageName, String keyWord, String content, int parentPageId) {
-		super();
+	
+	
+	
+	public Page(String id, String pageName, int parentPageId) {
 		this.id = id;
 		this.pageName = pageName;
-		this.keyWord = keyWord;
-		this.content = content;
 		this.parentPageId = parentPageId;
 	}
-	
+
 	public static List<Page> getSamplePages() {
-        return Arrays.asList(
-            new Page("1", "Home", "home, main", "Welcome to Home Page", 0),
-            new Page("2", "About", "about, info", "About Us Page Content", 0),
-            new Page("3", "Contact", "contact, support", "Contact Us Page", 0),
-            new Page("4", "Services", "services, offerings", "Our Services Page", 0),
-            new Page("5", "Blog", "blog, articles", "Our Blog Page", 0),
-            new Page("6", "FAQ", "faq, questions", "Frequently Asked Questions", 0),
-            new Page("7", "Privacy Policy", "privacy, terms", "Privacy Policy Page", 0),
-            new Page("8", "Careers", "careers, jobs", "Join our team!", 0),
-            new Page("9", "Portfolio", "portfolio, work", "Our Work Showcase", 0),
-            new Page("10", "Support", "support, help", "Support and Help Center", 0)
-        );
+        List<Page> pages = new ArrayList<Page>();
+        pages.add(new Page("1", "Home", 0));
+        pages.add(new Page("2", "About", 0));
+        pages.add(new Page("3", "Contact", 0));
+        return pages;
     }
 
     public static void main(String[] args) {
