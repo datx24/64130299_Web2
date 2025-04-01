0a575e4844bc74d2aace58844c7c9bee34c06811
datx24 <xuandat475@gmail.com>
2025-04-01 09:44:05 +0700

    push code lần 5

--- NguyenXuanDat_ThiGK/src/main/java/thiGK/ntu64130299/controllers/DashBoardController.java
+++ NguyenXuanDat_ThiGK/src/main/java/thiGK/ntu64130299/controllers/DashBoardController.java
@@ -1,8 +1,12 @@
 package thiGK.ntu64130299.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import thiGK.ntu64130299.model.Page;
 
 @Controller
 public class DashBoardController {
@@ -10,4 +14,12 @@ public class DashBoardController {
     public String showDashboard() {
         return "page/all";
     }
+	
+	 @GetMapping("/page/all")
+	    public String getAllPages(Model model) {
+	        List<Page> pages = Page.getSamplePages();
+	        model.addAttribute("pages", pages); 
+	        return "page/all";
+	    }
+	
 }
