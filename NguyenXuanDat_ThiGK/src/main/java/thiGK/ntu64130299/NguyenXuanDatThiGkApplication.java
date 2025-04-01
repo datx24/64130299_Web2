75404076aaf7a8fd2a18c57271cb0875e63dcd4e
datx24 <xuandat475@gmail.com>
2025-04-01 09:04:17 +0700

    tạo dự án thi cuối kì

---
+++ NguyenXuanDat_ThiGK/src/main/java/thiGK/ntu64130299/NguyenXuanDatThiGkApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64130299;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenXuanDatThiGkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenXuanDatThiGkApplication.class, args);
+	}
+
+}
---
+++ NguyenXuanDat_ThiGK/src/test/java/thiGK/ntu64130299/NguyenXuanDatThiGkApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64130299;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenXuanDatThiGkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
