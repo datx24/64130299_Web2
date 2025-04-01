25c679642d2d9b4521bcb200b286344d82205673
datx24 <xuandat475@gmail.com>
2025-04-01 09:24:43 +0700

    hard-code page

--- NguyenXuanDat_ThiGK/src/main/java/thiGK/ntu64130299/model/Page.java
+++ NguyenXuanDat_ThiGK/src/main/java/thiGK/ntu64130299/model/Page.java
@@ -1,5 +1,8 @@
 package thiGK.ntu64130299.model;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Page {
 	private String id;
 	private String pageName;
@@ -14,6 +17,28 @@ public class Page {
 		this.content = content;
 		this.parentPageId = parentPageId;
 	}
+	
+	public static List<Page> getSamplePages() {
+        return Arrays.asList(
+            new Page("1", "Home", "home, main", "Welcome to Home Page", 0),
+            new Page("2", "About", "about, info", "About Us Page Content", 0),
+            new Page("3", "Contact", "contact, support", "Contact Us Page", 0),
+            new Page("4", "Services", "services, offerings", "Our Services Page", 0),
+            new Page("5", "Blog", "blog, articles", "Our Blog Page", 0),
+            new Page("6", "FAQ", "faq, questions", "Frequently Asked Questions", 0),
+            new Page("7", "Privacy Policy", "privacy, terms", "Privacy Policy Page", 0),
+            new Page("8", "Careers", "careers, jobs", "Join our team!", 0),
+            new Page("9", "Portfolio", "portfolio, work", "Our Work Showcase", 0),
+            new Page("10", "Support", "support, help", "Support and Help Center", 0)
+        );
+    }
+
+    public static void main(String[] args) {
+        List<Page> pages = getSamplePages();
+        for (Page page : pages) {
+            System.out.println("ID: " + page.getId() + ", Name: " + page.getPageName());
+        }
+    }
 	public String getId() {
 		return id;
 	}
