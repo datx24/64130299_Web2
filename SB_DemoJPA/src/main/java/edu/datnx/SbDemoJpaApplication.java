80a7916df8c34555b16790a8789573d1ea7dfafe
datx24 <xuandat475@gmail.com>
2025-04-22 10:06:13 +0700

    tạo dự án demo jpa

---
+++ SB_DemoJPA/src/main/java/edu/datnx/SbDemoJpaApplication.java
@@ -0,0 +1,13 @@
+package edu.datnx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDemoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDemoJpaApplication.class, args);
+	}
+
+}
